bebaac100aff0a2aa31dc8ce6b81ed16d62d019c
Paul Woolcock <paul@woolcock.us>
2015-08-12 13:09:01 -0400

    Change a couple fields into Enums

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
-
 #![feature(ip_addr)]
-
 #[macro_use] extern crate nom;
+
+/// DHCP Protocol, RFC-2131
+/// 
+/// This crate implements DHCP message parsing, compatible with RFC 2131
+
 use std::str;
 use std::fmt;
 use std::borrow::{ToOwned};
 use std::error;
 use std::convert::{From};
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use std::net::{IpAddr, Ipv4Addr};
 use nom::{IResult, be_u8, be_u16, be_u32};
 
 fn take_rest(input: &[u8]) -> IResult<&[u8], &[u8]> {
@@ -39,10 +42,73 @@ impl error::Error for Error {
     }
 }
 
-#[derive(Debug)]
+pub type Result<T> = ::std::result::Result<T, Error>;
+
+#[derive(Debug, PartialEq)]
+pub enum Op {
+    BootRequest = 1,
+    BootReply,
+}
+
+impl Op {
+    fn from_byte(byte: u8) -> Result<Op> {
+        match byte {
+            1u8 => { Ok(Op::BootRequest) },
+            2u8 => { Ok(Op::BootReply) },
+            _ => { Err(Error::ParseError("Got bad value for `op`".into())) }
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+#[allow(non_camel_case_types)]
+pub enum Htype {
+    Ethernet_10mb = 1,
+    Experimental_Ethernet_3mb,
+    Amateur_Radio_AX_25,
+    Proteon_ProNET_Token_Ring,
+    Chaos,
+    IEEE_802_Networks,
+    Arcnet,
+    Hyperchannel,
+    Lanstar,
+    Autonet_Short_Address,
+    LocalTalk,
+    LocalNet,
+    Ultra_link,
+    SMDS,
+    Frame_Relay,
+    Asynchronous_Transmission_Mode,
+}
+
+impl Htype {
+    fn from_byte(byte: u8) -> Result<Htype> {
+        match byte {
+            1u8 => Ok(Htype::Ethernet_10mb),
+            2u8 => Ok(Htype::Experimental_Ethernet_3mb),
+            3u8 => Ok(Htype::Amateur_Radio_AX_25),
+            4u8 => Ok(Htype::Proteon_ProNET_Token_Ring),
+            5u8 => Ok(Htype::Chaos),
+            6u8 => Ok(Htype::IEEE_802_Networks),
+            7u8 => Ok(Htype::Arcnet),
+            8u8 => Ok(Htype::Hyperchannel),
+            9u8 => Ok(Htype::Lanstar),
+            10u8 => Ok(Htype::Autonet_Short_Address),
+            11u8 => Ok(Htype::LocalTalk),
+            12u8 => Ok(Htype::LocalNet),
+            13u8 => Ok(Htype::Ultra_link),
+            14u8 => Ok(Htype::SMDS),
+            15u8 => Ok(Htype::Frame_Relay),
+            16u8 => Ok(Htype::Asynchronous_Transmission_Mode),
+            _ => Err(Error::ParseError(format!("Unknown Htype {:?}", byte)))
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub struct Message<'a> {
-    op: u8,
-    htype: u8,
+    op: Op,
+    htype: Htype,
     hlen: u8,
     hops: u8,
     xid: u32,
@@ -58,15 +124,92 @@ pub struct Message<'a> {
     options: Option<Vec<u8>>,
 }
 
-fn null_terminated_slice_to_string(bytes: &[u8]) -> Result<&str, String> {
+fn null_terminated_slice_to_string(bytes: &[u8]) -> Result<&str> {
     let pos = match bytes.iter().position(|b| *b == 0u8) {
         Some(p) => p,
-        None => return Err("NO NULL TERMINATION FOUND".into()),
+        None => return Err(Error::ParseError("NO NULL TERMINATION FOUND".into())),
     };
     match str::from_utf8(&bytes[0..pos]) {
         Ok(s) => Ok(s),
-        Err(_) => Err("Could not get utf8 from bytes".into()),
+        Err(_) => Err(Error::ParseError("Could not get utf8 from bytes".into())),
     }
+}
+
+fn parse_message<'a>(bytes: &'a [u8]) -> Result<Message<'a>> {
+    match _parse_message(bytes) {
+        IResult::Done(inp, msg) => {
+            if inp.len() > 0 {
+                return Err(Error::ParseError("LEFTOVER INPUT".into()));
+            }
+            Ok(msg)
+        },
+        IResult::Error(err) => {
+            Err(Error::ParseError(format!("SOME OTHER ERROR: {:?}", err)))
+        },
+        IResult::Incomplete(_) => {
+            Err(Error::ParseError("INCOMPLETE".into()))
+        }
+    }
+}
+
+named!(_parse_message(&'a [u8]) -> Message<'a>,
+    chain!(
+        pop: map_res!(be_u8, Op::from_byte) ~
+        phtype: map_res!(be_u8, Htype::from_byte) ~
+        phlen: be_u8 ~
+        phops: be_u8 ~
+        pxid: be_u32 ~
+        psecs: be_u16 ~
+        pflags: be_u16 ~
+        pciaddr: be_u32 ~
+        pyiaddr: be_u32 ~
+        psiaddr: be_u32 ~
+        pgiaddr: be_u32 ~
+        pchaddr: take!(16) ~
+        psname: map_res!(take!(64), null_terminated_slice_to_string) ~
+        pfile: map_res!(take!(128), null_terminated_slice_to_string) ~
+        poptions: take_rest,
+    ||{
+        Message {
+            op: pop,
+            htype: phtype,
+            hlen: phlen,
+            hops: phops,
+            xid: pxid,
+            secs: psecs,
+            flags: pflags,
+            ciaddr: IpAddr::V4(Ipv4Addr::from(pciaddr)),
+            yiaddr: IpAddr::V4(Ipv4Addr::from(pyiaddr)),
+            siaddr: IpAddr::V4(Ipv4Addr::from(psiaddr)),
+            giaddr: IpAddr::V4(Ipv4Addr::from(pgiaddr)),
+            chaddr: pchaddr.to_owned(),
+            sname: psname,
+            file: pfile,
+            options: Some(poptions.to_owned()),
+        }
+    }
+    )
+);
+
+#[cfg(test)]
+mod tests {
+
+    use std::str;
+    use nom::IResult;
+    use super::{take_rest, parse_message, Message, Op, Htype};
+
+#[test]
+fn test_take_rest() {
+    named!(parts<&[u8],(&str,&str)>,
+        chain!(
+            key: map_res!(tag!("abcd"), str::from_utf8) ~
+            tag!(":") ~
+            value: map_res!(take_rest, str::from_utf8),
+            || {(key, value)}
+        )
+    );
+
+    assert_eq!(parts(b"abcd:thisistherestofthestring"), IResult::Done(&b""[..], ("abcd", "thisistherestofthestring")));
 }
 
 #[test]
@@ -117,75 +260,24 @@ fn test_parse_message() {
         119, 120, 121, 122, 123, 124, 125, 109,
         0, 0, 0, 0, 0, 0, 0, 0, // file
     ];
-    println!("{:?}", parse_message(&test_message).unwrap());
+    assert_eq!(parse_message(&test_message).unwrap(), Message {
+        op: Op::BootRequest,
+        htype: Htype::Experimental_Ethernet_3mb,
+        hlen: 3,
+        hops: 4,
+        xid: 84281096,
+        secs: 2314,
+        flags: 2828,
+        ciaddr: str::FromStr::from_str("13.14.15.16").unwrap(),
+        yiaddr: str::FromStr::from_str("17.18.19.20").unwrap(),
+        siaddr: str::FromStr::from_str("21.22.23.24").unwrap(),
+        giaddr: str::FromStr::from_str("25.26.27.28").unwrap(),
+        chaddr: vec![29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+        sname: "-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijk",
+        file: "mnopqrstuvwxyz{|}mnopqrstuvwxyz{|}mnopqrstuvwxyz{|}mnopqrstuvwxyz{|}mnopqrstuvwxyz{|}mnopqrstuvwxyz{|}mnopqrstuvwxyz{|}m",
+        options: Some(vec![])
+    });
+
 }
 
-fn parse_message<'a>(bytes: &'a [u8]) -> Result<Message<'a>, Error> {
-    match _parse_message(bytes) {
-        IResult::Done(inp, msg) => {
-            if inp.len() > 0 {
-                return Err(Error::ParseError("LEFTOVER INPUT".into()));
-            }
-            Ok(msg)
-        },
-        IResult::Error(err) => {
-            Err(Error::ParseError(format!("SOME OTHER ERROR: {:?}", err)))
-        },
-        IResult::Incomplete(_) => {
-            Err(Error::ParseError("INCOMPLETE".into()))
-        }
-    }
-}
-
-named!(_parse_message(&'a [u8]) -> Message<'a>,
-    chain!(
-        pop: be_u8 ~
-        phtype: be_u8 ~
-        phlen: be_u8 ~
-        phops: be_u8 ~
-        pxid: be_u32 ~
-        psecs: be_u16 ~
-        pflags: be_u16 ~
-        pciaddr: be_u32 ~
-        pyiaddr: be_u32 ~
-        psiaddr: be_u32 ~
-        pgiaddr: be_u32 ~
-        pchaddr: take!(16) ~
-        psname: map_res!(take!(64), null_terminated_slice_to_string) ~
-        pfile: map_res!(take!(128), null_terminated_slice_to_string) ~
-        poptions: take_rest,
-    ||{
-        Message {
-            op: pop,
-            htype: phtype,
-            hlen: phlen,
-            hops: phops,
-            xid: pxid,
-            secs: psecs,
-            flags: pflags,
-            ciaddr: IpAddr::V4(Ipv4Addr::from(pciaddr)),
-            yiaddr: IpAddr::V4(Ipv4Addr::from(pyiaddr)),
-            siaddr: IpAddr::V4(Ipv4Addr::from(psiaddr)),
-            giaddr: IpAddr::V4(Ipv4Addr::from(pgiaddr)),
-            chaddr: pchaddr.to_owned(),
-            sname: psname,
-            file: pfile,
-            options: Some(poptions.to_owned()),
-        }
-    }
-    )
-);
-
-#[test]
-fn test_take_rest() {
-    named!(parts<&[u8],(&str,&str)>,
-        chain!(
-            key: map_res!(tag!("abcd"), str::from_utf8) ~
-            tag!(":") ~
-            value: map_res!(take_rest, str::from_utf8),
-            || {(key, value)}
-        )
-    );
-
-    assert_eq!(parts(b"abcd:thisistherestofthestring"), IResult::Done(&b""[..], ("abcd", "thisistherestofthestring")));
 }

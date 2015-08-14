8c1671f70bbff42e594bce6d608ab651323456d6
Paul Woolcock <paul@woolcock.us>
2015-08-14 14:20:26 -0400

    remove the actual message parsing to its own crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,6 @@
-#![feature(ip_addr)]
-#[macro_use] extern crate nom;
-#[macro_use] extern crate enum_primitive;
-extern crate num;
-
 /// DHCP Protocol, RFC-2131
 /// 
 /// This crate implements DHCP message parsing, compatible with RFC 2131
 
-mod message;
-
-pub use message::Message;
+extern crate dhcp_parser;
 
